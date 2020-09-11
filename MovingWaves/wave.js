68c0ee25d830af537a2bddd85d22ae4819239481
Andy Park <chanhyp1@uci.edu>
2020-09-10 22:46:09 -0700

    Add one more moving up and down

---
+++ MovingWaves/wave.js
@@ -0,0 +1,34 @@
+import { Point } from "./point.js";
+
+export class Wave {
+   constructor() {
+
+   }
+
+   resize(stageWidth, stageHeight) {
+      this.stageWidth = stageWidth;
+      this.stageHeight = stageHeight;
+
+      this.centerX = this.stageWidth / 2;
+      this.centerY = this.stageHeight / 2;
+
+      this.init();
+   }
+
+   init() {
+      this.point = new Point(
+         this.centerX,
+         this.centerY
+      );
+   }
+
+   draw(ctx) {
+      ctx.beginPath();
+      ctx.fillStyle='#ff000';
+
+      this.point.update();
+
+      ctx.arc(this.point.x, this.point.y, 30, 0, 2 * Math.PI);
+      ctx.fill();
+   }
+}
